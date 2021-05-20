f74b36ca922ea15e2751ebe9c037dbf4d9209cab
djalal <djalal@users.noreply.github.com>
2021-05-20 18:26:46 +0200

    add .artifakt folder

---
+++ .artifakt/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "BUILD.SH CUSTOM SCRIPT - BEGIN >>>>>>>>"
+
+env
+
+echo "BUILD.SH CUSTOM SCRIPT - END <<<<<<<<<"
---
+++ .artifakt/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "ENTRYPOINT.SH CUSTOM SCRIPT - BEGIN >>>>>>>>"
+
+env
+
+echo "ENTRYPOINT.SH CUSTOM SCRIPT - END <<<<<<<<<"
