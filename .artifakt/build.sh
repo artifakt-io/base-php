c775c77259fd9920e910355af5219fcf2c4c9a13
djalal <hi@djal.al>
2021-06-05 18:39:48 +0200

    complete local setup

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -2,6 +2,21 @@
 
 echo "BUILD.SH CUSTOM SCRIPT - BEGIN >>>>>>>>"
 
+RUNTIME_NAME="PHP"
+RUNTIME_LOGO="php.png"
+ARCHIVE_FILE=static-page-main
+
 env
+
+curl -sSLO https://gitlab.com/djalal/static-page/-/archive/main/${ARCHIVE_FILE}.tar.gz && \
+  tar -xzf ${ARCHIVE_FILE}.tar.gz -C /tmp && \
+  mv /tmp/${ARCHIVE_FILE}/* /var/www/html && \
+  chown -R www-data:www-data /var/www/html && \
+  rm ${ARCHIVE_FILE}.tar.gz
+
+sed -i "s/__RUNTIME_NAME__/${RUNTIME_NAME}/" /var/www/html/index.html
+sed -i "s/__RUNTIME_LOGO__/${RUNTIME_LOGO}/" /var/www/html/index.html
+
+rm /var/www/html/index.php
 
 echo "BUILD.SH CUSTOM SCRIPT - END <<<<<<<<<"
