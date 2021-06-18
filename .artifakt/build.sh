124595bcb7483780492c5dea9061db0ae02bd61c
djalal <djalal@users.noreply.github.com>
2021-06-18 12:08:31 +0200

    fix welcome page

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-echo "BUILD.SH CUSTOM SCRIPT - BEGIN >>>>>>>>"
+set -e
 
+echo ">>>>>>>>>>>>>> START CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
+echo "------------------------------------------------------------"
+echo "The following build args are available:"
 env
+echo "------------------------------------------------------------"
 
-echo "BUILD.SH CUSTOM SCRIPT - END <<<<<<<<<"
+RUNTIME_NAME="PHP"
+RUNTIME_LOGO="php.png"
+REPO_NAME=base-html
+REPO_BRANCH=main
+ARCHIVE_FILE=$REPO_NAME-$REPO_BRANCH
+
+curl -sSLO https://github.com/artifakt-io/${REPO_NAME}/archive/refs/heads/${REPO_BRANCH}.tar.gz && \
+	tar -xzf ${REPO_BRANCH}.tar.gz -C /tmp && \
+	mv /tmp/${ARCHIVE_FILE}/* /var/www/html && \
+	chown -R www-data:www-data /var/www/html && \
+	rm ${REPO_BRANCH}.tar.gz
+
+sed -i "s/__RUNTIME_NAME__/${RUNTIME_NAME}/" /var/www/html/index.html
+sed -i "s/__RUNTIME_LOGO__/${RUNTIME_LOGO}/" /var/www/html/index.html
+
+rm /var/www/html/index.php
+
+echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
